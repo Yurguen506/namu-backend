fbe1c6798fd2f4263ab1b23ac472e964bdde702a
Yurguen Campos Sánchez <yurguen.campos@ucrso.info>
2022-07-15 10:12:58 -0600

    backend ready with external hosting

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "Sequelize";
 
-const db = new Sequelize ("EsenciaNamu", "sa", "." ,
+const db = new Sequelize ("EsenciaNamu", "Jurgen506_SQLLogin_1", "2yyuwlqwbm" ,
     {
-        host: 'localhost',
+        host: 'EsenciaNamu.mssql.somee.com',
         dialect: 'mssql'
     }
 );
