2c40ccfd9be57bb099d5b6b7be73769296d1188e
Yurguen Campos Sánchez <yurguen.campos@ucrso.info>
2022-07-22 23:35:48 -0600

    add method to imageController 2

--- models/ImageModel.js
+++ models/ImageModel.js
@@ -7,7 +7,7 @@ const ImagesModel = db.define('images',
         //ID : {type: DataTypes.INTEGER},
         idPlace : {type: DataTypes.INTEGER},
         idPhotographer : {type: DataTypes.INTEGER},
-        image : {type: DataTypes.BLOB}
+        image : {type: DataTypes.STRING}
     }, 
     {
         timestamps: false,
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getAllPlaces, getPlaceById } from "../controllers/PlaceController.js";
 import { getAllPhotographers, getPhotographerById } from "../controllers/PhotographerController.js";
-import { previewPlacesImages } from "../controllers/ImageController.js";
+import { previewPlacesImages, getAllImages } from "../controllers/ImageController.js";
 import { getAllCoordinates, getCoordinateById } from "../controllers/CoordinateController.js";
 
 const router = express.Router();
@@ -17,7 +17,7 @@ router.get( "/coordinate/:id" , getCoordinateById)
 
 
 router.get ('/image/:id', previewPlacesImages)
-router.get("/images/", getAllImages);
+router.get("/images/", getAllImages)
 
 // router.post ('/:id/upload', uploadFile.single("file"), uploadBlogImage)
 
